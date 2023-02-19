ec6f61742da2bd18ba6ccaf7284ef122d9301b79
Dat Tan <dattan2911@gmail.com>
2023-02-19 14:29:50 +0700

    hot fix

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path';
 import dotenv from 'dotenv';
 import productRoute from './Router/productRoute.js';
 import userRoute from './Router/userRoute.js';
@@ -32,6 +33,11 @@ app.get('/api/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
